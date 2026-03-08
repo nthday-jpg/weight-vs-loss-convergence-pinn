0dc85f02df7edf47f9b4124a6d42324e5f0d48c7
nthday-jpg <24127166@student.hcmus.edu.vn>
2026-03-08 23:05:42 +0700

    Enhance weight balancing logic with clamping and renormalization

--- balancer/inv.py
+++ balancer/inv.py
@@ -14,7 +14,7 @@ class Balancer:
         }
     
     def balance(self, loss_dict):
-        """Adjust weights inversely to losses with max-normalization."""
+        """Adjust weights inversely to losses with max-normalization and clamping."""
         ics_val = loss_dict['ics_loss'].detach().item()
         bcs_val = loss_dict['bcs_loss'].detach().item()
         res_val = loss_dict['res_loss'].detach().item()
@@ -32,9 +32,22 @@ class Balancer:
         weight_bcs_raw = inv_bcs / inv_sum
         weight_res_raw = inv_res / inv_sum
         
+        # Apply EMA smoothing
         self.weights['ics'] = self.alpha * weight_ics_raw + (1 - self.alpha) * self.weights['ics']
         self.weights['bcs'] = self.alpha * weight_bcs_raw + (1 - self.alpha) * self.weights['bcs']
         self.weights['res'] = self.alpha * weight_res_raw + (1 - self.alpha) * self.weights['res']
+        
+        # Clamp weights to minimum threshold
+        min_weight = 0.15
+        self.weights['ics'] = max(self.weights['ics'], min_weight)
+        self.weights['bcs'] = max(self.weights['bcs'], min_weight)
+        self.weights['res'] = max(self.weights['res'], min_weight)
+        
+        # Renormalize to sum to 1.0
+        weight_sum = self.weights['ics'] + self.weights['bcs'] + self.weights['res']
+        self.weights['ics'] /= weight_sum
+        self.weights['bcs'] /= weight_sum
+        self.weights['res'] /= weight_sum
 
     def __call__(self, loss_dict):
         self.balance(loss_dict)
